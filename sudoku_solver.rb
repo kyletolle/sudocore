6120015e6c9864df4e600119afcf47ac3c54e311
Kyle Tolle <kyle@nullsix.com>
2011-09-01 21:23:49 -0600

    Added in option parsing. Accepts -h and --help switches

--- sudoku_solver.rb
+++ sudoku_solver.rb
@@ -1,33 +1,52 @@
+require 'optparse'
+
 class Sudoku
   
-  /**
-   * Parses sudoku puzzle file and builds data structure for solving.
-   * @parm string file path to file containing sudoku puzzle
-   */
+  # Parses sudoku puzzle file and builds data structure for solving.
+  # @parm string file path to file containing sudoku puzzle
   def initialize(file)
     @puzzle = []
   end
-
-  /**
-   * Solves sudoku puzzle
-   */
+  
+  #
+  # Solves sudoku puzzle
   def solve
     
   end
-
-  /**
-   * Returns string representation of sudoku puzzle for printing
-   * @return string
-   */
+  
+  # Returns string representation of sudoku puzzle for printing
+  # @return string
   def to_s
-    return '';
+    return 'TODO: Implement Sudoku#to_s';
   end
 end
 
-# Need to have some stuff for printing options here.
+# TODO: Parse command line options
+# Hold the parsed options
+options = {}
 
-file = "some file from the options"
+# Define the options and their uses
+optparse = OptionParser.new do |opts|
+  # Set a banner message at the top of the help screen
+  opts.banner = "Usage: ruby sudoku-solver.rb [options] file"
 
-sudoku = Sudoku.new(file)
+  #TODO: Need to handle invalid options and the exceptions thrown by
+  # OptionParser
+
+  #TODO: Perhaps use a flag here for whether it's numeric or hex
+
+  # Displays the help screen
+  opts.on('-h', '--help', 'Display this help screen') do
+    puts opts
+    exit
+  end
+end
+
+# Parse the command line, removing any options, leaving the filename we want
+optparse.parse!
+
+filename = ARGV[0]
+
+sudoku = Sudoku.new(filename)
 sudoku.solve
-puts soduku
+puts sudoku
