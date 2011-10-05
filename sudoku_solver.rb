11a398508ae7a8409627540f23103e63e3a845a4
Kyle Tolle <kyle@nullsix.com>
2011-10-05 15:11:54 -0600

    Puzzle now made up of Cells with values. Puzzle#to_s returns a pretty string to print

--- sudoku_solver.rb
+++ sudoku_solver.rb
@@ -1,43 +1,66 @@
 require 'optparse'
 
 class Sudoku
-
+  
   DEC_REGEX = /^[0-9]$/
   HEX_REGEX = /^[a-zA-z0-9]$/
   BLANKS_REGEX = /^[_]$/
-  
-  # Names of classes from: http://en.wikipedia.org/wiki/Sudoku#Terminology
-  #
-  class Puzzle
-    # Is a n*n array of Cells
-    #TODO: How do I add cells to the puzzle?
 
-    class Cell
-  
-      def initialize(char)
-        #TODO: Check to see if char matches the digit or blank regexes.
-        #If so, store it. If not, raise error.
-        #
+  ## Names of classes from: http://en.wikipedia.org/wiki/Sudoku#Terminology
+  ##
+  class Cell
+
+    def initialize(char)
+      #
+      #TODO: Check to see if char matches the digit or blank regexes.
+      #If so, store it. If not, raise error.
+      #
+      #TODO: How do we see what base the Cell should be in based on Sudoku's base?
+      if char =~ DEC_REGEX or char =~ BLANKS_REGEX
+        # Make all blank characters a period for readability in output.
+        if char =~ BLANKS_REGEX
+          char.replace(".")
+        end
+        @char = char
+      else
+        raise ArgumentError, "Each character of the Sudoku puzzle's cells must be a valid character or blank entry."
       end
-  
-      def blank?
-        return # whether char matches blanks regex
-      end
-  
+
     end
-  
-    class Row
+
+
+    def blank?
+
+      #TODO: How do we see what base the Cell should be in based on Sudoku's base?
+      return @char =~ BLANKS_REGEX # whether char matches blanks regex
+
     end
-  
-    class Column
+
+
+    def to_s
+
+      @char.to_s
+
     end
-  
-    class Nonet
-    end
-    
-    class House
-    end
+
   end
+
+
+  class Row
+  end
+
+
+  class Column
+  end
+
+
+  class Nonet
+  end
+  
+
+  class House
+  end
+
 
   #TODO: How can I make options optional and also a hash?
   # Parses sudoku puzzle file and builds data structure for solving.
@@ -46,45 +69,26 @@ class Sudoku
   def initialize(filename)
     
     #TODO: Add whether we want to use decimal or hex digits.
-      @digit_regex = DEC_REGEX
+    @digit_regex = DEC_REGEX
 
     # Characters to represent valid blank characters in the puzzle
     @blank_regex = BLANKS_REGEX
 
     ## Read and parse in the puzzle now
-
+    ##
     #TODO: I think I'll want to use a puzzle class to abstract things away.
     @puzzle = [] #Puzzle.new
+    @current_row = nil
 
     # Read in the file with the puzzle
     File.open(filename) do |file|
-      line_count = 0
       file.each do |line|
-        chars = []
         line.chomp.each_char do |char| 
-          # Make sure we have either valid digits or blanks
-          unless char =~ @digit_regex or char =~ @blank_regex
-            #TODO: Move this error into a class inside Puzzle.
-            raise RuntimeError, "Each character of the Sudoku puzzle must be a valid character or blank entry." 
-          end
-
-          chars << char
+          # Add this cell to the puzzle
+          add_cell(Cell.new(char))
         end
 
-        # Rows must have 9 characters
-        #TODO: Move this error into a class inside Puzzle.
-        unless chars.count == 9
-          raise RuntimeError, "Each row in a Sudoku puzzle must have 9 columns."
-        end
-
-        @puzzle << chars
-        line_count += 1
-      end
-      
-      # Puzzle must have 9 rows
-      #TODO: Move this error into a class inside Puzzle.
-      unless line_count == 9
-        raise RuntimeError, "Sudoku puzzle must have 9 rows."
+        add_row
       end
     end
 
@@ -99,7 +103,7 @@ class Sudoku
       exit
 
     ## Catch file errors ##
-
+    ##
     rescue Errno::ENOENT
       puts "Error: Couldn't find the sudoku file."
     
@@ -113,6 +117,7 @@ class Sudoku
     @puzzle.each do |row| p row end
   end
   
+
   # Solves sudoku puzzle
   def solve
     #TODO: Determine which algorithm to use to solve the puzzle
@@ -124,22 +129,78 @@ class Sudoku
     #TODO: Then figure out how to abstract the algorithm away, so I can swap in a graph
     # coloring algorithm later!
 
-    puts 'TODO: Implement Sudoku#solve'
+    unless @puzzle.size == 9
+      raise RuntimeError, "Sudoku puzzle must have 9 rows."
+    end
+
+    if valid?
+      try_solve(0)
+    else
+      raise RuntimeError, "Sudoku puzzle contents were not valid."
+    end
+
     self
   end
   
+
   # Returns string representation of sudoku puzzle for printing
   # @return string
   def to_s
-    # Make a string of all the puzzle values
-    @puzzle.inject("") do |str1, row|
-      # Put all the values onto one line
-      row.inject(str1) {|str2, val| str2 + val } + "\n"
+    
+    # Make a pretty string of the puzzle
+    visual_puzzle = 
+    @puzzle.each_with_index.inject("") do |puzzle_string, (row, row_num)|
+      if (row_num+1) % 3 == 0 and row_num < 8
+        row_sep = " - - - + - - - + - - -\n"
+      else
+        row_sep = ""
+      end
+
+      row.each_with_index.inject(puzzle_string) do |row_string, (cell, cell_num)|
+          if (cell_num+1) % 3 == 0 and cell_num < 8
+            sep = " |"
+          else
+            sep = ""
+          end
+
+          row_string + " " + cell.to_s + sep
+        end + "\n" + row_sep
     end
+
+    "\n" + visual_puzzle + "\n"
+
   end
 
-  private
+
+  def add_row
+
+    unless @current_row.nil?
+      @puzzle << @current_row
+      @current_row = nil
+    end
+
+  end
+
+
+  def add_cell(cell)
+
+    @current_row ||= []
+    
+    if @current_row.size < 9
+      unless cell.class == Cell.class
+        @current_row << cell
+      else
+        raise RuntimeError, "Must add a cell to the Sudoku puzzle"
+      end
+    else
+      raise RuntimeError, "Sudoku puzzle can't have more than 9 Cells in a row. You added #{@current_row.size} elements."
+    end
+
+  end
+
+
   def try_solve(index)
+
     # if already filled in
     #   try_solve(next index)
     #   return solved?
@@ -159,6 +220,12 @@ class Sudoku
     #       next
 
   end
+
+  def valid?
+    return true #TODO: Find out if there are conflicts in row, column or nonet.
+  end
+
+  private :add_row, :add_cell, :try_solve, :valid?
 end
 
 # Hold the parsed options
