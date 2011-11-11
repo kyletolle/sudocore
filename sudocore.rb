ac76eb96dc58cd02609aa1647686b2108a9472c6
Kyle Tolle <kyle@nullsix.com>
2011-11-10 20:39:30 -0700

    Fixes #26. User killing the program with Ctrl-C doesn't print out an ugly eception.

--- sudocore.rb
+++ sudocore.rb
@@ -1,29 +1,34 @@
 ## Sudocore - A Sudoku solver written in Ruby.
 ##
 
-# Parse the command line options and get the filename that holds the puzzle.
-require './command_line_parser'
-cmd_parser = CommandLineParser.new
-options = cmd_parser.parse!
+# Prevents ugly error from a user killing the program.
+begin
+  # Parse the command line options and get the filename that holds the puzzle.
+  require './command_line_parser'
+  cmd_parser = CommandLineParser.new
+  options = cmd_parser.parse!
 
-# Create the puzzle with the file.
-require './puzzle'
-puzzle = Puzzle.new(options.file, options.debug)
+  # Create the puzzle with the file.
+  require './puzzle'
+  puzzle = Puzzle.new(options.file, options.debug)
 
-# Set up timer with whether user wants to log time info, and to what detail.
-require './timer'
-timer = Timer.new(options.time)
+  # Set up timer with whether user wants to log time info, and to what detail.
+  require './timer'
+  timer = Timer.new(options.time)
 
 
-# Write out the puzzle before it is solved.
-puts "Unsolved puzzle:"
-puts "#{puzzle.to_s}"
+  # Write out the puzzle before it is solved.
+  puts "Unsolved puzzle:"
+  puts "#{puzzle.to_s}"
 
-timer.start
-puzzle.solve
-timer.stop
+  timer.start
+  puzzle.solve
+  timer.stop
 
-# Write out the puzzle after it is solved.
-puts "Solved puzzle:"
-puts "#{puzzle.to_s}"
+  # Write out the puzzle after it is solved.
+  puts "Solved puzzle:"
+  puts "#{puzzle.to_s}"
 
+rescue Interrupt
+  exit
+end
